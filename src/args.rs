f1444a0028225eeb25f4b1dc2e000bbc9082dc44
Fred Hawk <fredrichawk@gmail.com>
2022-06-18 09:01:11 +0200

    feat: close #17 Second arg optional.

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-#[clap(author, version, about)]
+#[clap(author="Fred", version, about="URL shortener")]
 pub struct AlliumArgs {
     /// The long version of the url
+    #[clap(short='l', long="long")]
     pub long_url: String,
+
     /// An optional short version of the url
-    pub short_url: String,
+    #[clap(short='s', long="short")]
+    pub short_url: Option<String>,
 }
