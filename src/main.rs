2bcff8fe133303d2697a510df04e92d7d1ced0b1
Fred Hawk <fredrichawk@gmail.com>
2022-06-21 20:13:40 +0200

    feat: close #16 Check if the url exists in the file already.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use clap::Parser;
 use std::fs::File;
 use std::path::Path;
 use std::io::Write;
+use std::io::BufReader;
+use std::io::BufRead;
 use is_url::is_url;
 
 fn main() -> std::io::Result<()> {
@@ -21,22 +23,30 @@ fn main() -> std::io::Result<()> {
 
     // Check so that the long is a url
     let valid_url = is_url(_long);
-    println!("{}", valid_url);
 
-    // Make the entry from the strings
-    let entry: String = "\n".to_owned() + "/" + _short + " " + "/" + _long;
-    println!("{}", entry);
+    if valid_url == true {
+        // Make the entry from the strings
+        let entry: String = "\n".to_owned() + "/" + _short + " " + "/" + _long;
 
-    // Check if file exists
-    let file_exists = Path::new("_redirects").exists();
-    if file_exists == true {
-        // If it exists open it and append whatever is in the _long reference
-        let mut f = File::options().append(true).open("_redirects")?;
-        f.write(entry.as_bytes())?;
-    } else {
-        // If it doesnt exist. Create the file and write the _long reference to it.
-        let mut f = File::create("_redirects")?;
-        f.write(entry.as_bytes())?;
+        // Check if file exists
+        let file_exists = Path::new("_redirects").exists();
+        if file_exists == true {
+            // If it exists open it and append whatever is in the _long reference
+            let mut f = File::options().append(true).open("_redirects")?;
+            let reader = BufReader::new(&f);
+            let url_exist = reader.lines().any(|x| &x.unwrap() == _long);
+            // for line in reader.lines() {
+            if url_exist == true {
+                panic!("URL already exists.");
+            };
+            // };
+
+            f.write(entry.as_bytes())?;
+        } else {
+            // If it doesnt exist. Create the file and write the _long reference to it.
+            let mut f = File::create("_redirects")?;
+            f.write(entry.as_bytes())?;
+        }
     }
 
     Ok(())
